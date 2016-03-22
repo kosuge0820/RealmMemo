7d9298df0b6f8261e5cc6ef39f33b8cce5bc8f49
小菅仁士 <satoshi082015@gmail.com>
2016-03-22 17:57:16 +0900

    initial commit

---
+++ RealmPractice/MemoManager.swift
@@ -0,0 +1,13 @@
+//
+//  MemoManager.swift
+//  RealmPractice
+//
+//  Created by 小菅仁士 on 2016/03/21.
+//  Copyright © 2016年 kosuge satoshi. All rights reserved.
+//
+
+import UIKit
+
+class MemoManager: NSObject {
+
+}
---
+++ RealmPractice/SearchTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SearchTableViewController.swift
+//  RealmPractice
+//
+//  Created by 小菅仁士 on 2016/03/21.
+//  Copyright © 2016年 kosuge satoshi. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController {
+    @IBOutlet weak var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        return cell
+    }
+}
